5a2ce1d7b70dbe1546f836a2cbcf8f3144108017
liu shiwei <liushiwei@gmail.com>
2021-03-30 11:27:06 +0800

    port.sh 同一个跳板， 可以多次把不同端口指向服务器的同一个端口

--- port.sh
+++ port.sh
@@ -23,11 +23,11 @@ echo 1 >/proc/sys/net/ipv4/ip_forward
 
 iptables -t nat -L -n >/tmp/${remote_ip}_${remote_port}.log
 
-if ! [ "`grep ^DNAT /tmp/${remote_ip}_${remote_port}.log |grep ${remote_ip} |grep ${remote_port} |grep ${local_ip}|grep tcp`" ] ; then
+if ! [ "`grep ^DNAT /tmp/${remote_ip}_${remote_port}.log |grep ${remote_ip} |grep ${remote_port} |grep $local_port |grep ${local_ip}|grep tcp`" ] ; then
  iptables -t nat -I PREROUTING -d $local_ip -p tcp --dport $local_port -j DNAT --to $remote_ip:$remote_port
 fi
 
-if ! [ "`grep ^DNAT /tmp/${remote_ip}_${remote_port}.log |grep ${remote_ip} |grep ${remote_port} |grep ${local_ip}|grep udp`" ] ; then
+if ! [ "`grep ^DNAT /tmp/${remote_ip}_${remote_port}.log |grep ${remote_ip} |grep ${remote_port} |grep $local_port |grep ${local_ip}|grep udp`" ] ; then
  iptables -t nat -I PREROUTING -d $local_ip -p udp --dport $local_port -j DNAT --to $remote_ip:$remote_port
 fi
 
