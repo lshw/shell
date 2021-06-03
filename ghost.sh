63148292e19d9d7f3db1505eb6b3836413c2e928
liu shiwei <liushiwei@gmail.com>
2021-06-03 13:54:26 +0800

    增加 ghost.sh 用于把当前linux系统打包成tar包

---
+++ ghost.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+
+dst_file="`uname -m`_`date +%Y%m%d`.tar.gz"
+echo make home/$dst_file
+echo proc/* >/tmp/exclude.list
+echo mnt/* >>/tmp/exclude.list
+echo tmp/* >>/tmp/exclude.list
+echo dev/* >>/tmp/exclude.list
+echo lost+found/* >>/tmp/exclude.list
+echo sys/* >>/tmp/exclude.list
+echo media/* >>/tmp/exclude.list
+echo run/* >>/tmp/exclude.list
+echo home/$dst_file >>/tmp/exclude.list
+cd /
+if [ "`which pv`" ] ; then
+  tar cp --exclude-from=/tmp/exclude.list * |pv|gzip >/home/$dst_file
+else
+ tar cpv --exclude-from=/tmp/exclude.list * |gzip >/home/$dst_file
+fi
