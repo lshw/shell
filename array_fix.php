9ca9e6e50fae62989fd073c1a21cf69c32ce218f
liu shiwei <liushiwei@gmail.com>
2024-05-14 20:27:04 +0800

    php 数组下标fix程序更新

--- array_fix.php
+++ array_fix.php
@@ -12,12 +12,23 @@ $f=$_SERVER['argv'][1];
 if (!file_exists($f)) {
     die("文件没有找到 $f\r\n");
 }
+$tempname=tempnam("/tmp", "array_fix_");
+if ($tempname == false) {
+    die("不能建立临时文件\r\n");
+}
+echo "$f ";
 $all=file($f);
 $t=0;
 $o='';
 $change = '';
 $skip = 0; //跳过注释
+$count=0;
 $lines=count($all);
+if(trim($all[0])=='<?') {
+    $all[0]=strtr($all[0],array("<?"=>"<?php"));
+    $change = '1';
+    $count++;
+}
 for ($i0=0; $i0<$lines; $i0++) {
     $a=$all[$i0];
     if (substr(trim($a), 0, 2)=='//') {
@@ -47,7 +58,8 @@ for ($i0=0; $i0<$lines; $i0++) {
         }
         switch ($t) {
             case 0: //先找'['
-                if ($bef == '/' and $c == '/') { //后面的不做处理
+                if (($c == '#' and $bef != "\\")
+                 or ($bef == '/' and $c == '/')) { //后面的不做处理
                     $o .= substr($a, $i, strlen($a));
                     $i=strlen($a);
                     break;
@@ -61,13 +73,46 @@ for ($i0=0; $i0<$lines; $i0++) {
                 $o.=$c;
                 if ($c=='[') {
                     //向前找$
-                    for ($i2 = $i-1; $i2 >= 0; $i2--) {
+                    $n1 = ''; //[]
+                    $n2 = ''; //"'
+                    for ($i2 = $i-1; $i2 >= 0; $i2--) { //先前找$符号
                         if ($a[$i2]=='$') {
-                            $t=1; //找到有效的数组
-                            $al=0; //数组key长度
-                            $begin=$i+1;
+                            if ($n1 == '' and  $n2 =='') { //'' "" [] 已经闭合
+                                $t=1; //找到有效的数组
+                                $al=0; //数组key长度
+                                $begin=$i+1;
+                            }
                         } elseif (!is_vname($a[$i2])) {
-                            break; //非有效数组名称， 跳过
+                            if ($a[$i2] != '"' and $a[$i2] != "'" and $a[$i2] != ']' and $a[$i2]!= '[') {
+                                break;
+                            }
+                            if ($n1 == '') { //第一个 '"] 则向前找第二个'"[
+                                if ($a[$i2] == ']') {
+                                    $n1='[';
+                                }
+                            } else { //找第二个[
+                                if ($a[$i2] == '[' and $n1 == '[') {
+                                    $n1=''; //[]闭合
+                                }
+                            }
+                            if ($n2 =='') { //找第一个 '' ""
+                                if ($a[$i2]=='"') {
+                                    $n2='"';
+                                } elseif ($a[$i2]=="'") {
+                                    $n2="'";
+                                }
+                            } else { //找第二个 '' ""
+                                if ($a[$i2] == '"' and $n2 != '"') {
+                                    break; //"与'交叉， 跳过
+                                } else {
+                                    $n2='';
+                                }
+                                if ($a[$i2]=="'" and $n2!="'") {
+                                    break;  //' "交叉, 跳过
+                                } else {
+                                    $n2=''; //找到
+                                }
+                            }
                         }
                     }
                 }
@@ -79,16 +124,16 @@ for ($i0=0; $i0<$lines; $i0++) {
                     if ($d=='' or is_numeric($d)) { //数组的key是数字，跳过不处理
                         $o.=$d.$c;
                     } else {
-                        file_put_contents("/tmp/$f.tmp", $o."'$d'".substr($a, $i));
+                        file_put_contents($tempname, $o."'$d'".substr($a, $i));
                         for ($i1=$i0+1; $i1<$lines; $i1++) {
-                            file_put_contents("/tmp/$f.tmp", $all[$i1], FILE_APPEND); //剩余行
+                            file_put_contents($tempname, $all[$i1], FILE_APPEND); //剩余行
                         }
                         $err=0;
-                        system("php -l /tmp/$f.tmp >/dev/null 2>/dev/null", $err);
-                        unlink("/tmp/$f.tmp");
+                        system("php -l $tempname >/dev/null 2>/dev/null", $err);
                         if ($err==0) { //php测试不出错， 增加单引号
                             $o.="'$d'$c";
                             $change=1;
+                            $count++;
                         } else {  //php测试出错， 当前的数组，可能在字符串中， 则不做处理
                             $o.="$d$c";
                         }
@@ -113,11 +158,17 @@ for ($i0=0; $i0<$lines; $i0++) {
 }
 if ($change == 1) {
     file_put_contents($f, $o);
+    echo "$count\r\n";
+} else {
+    echo "skip\r\n";
 }
+unlink($tempname);
+
 function is_vname($c)
 {
-                return $c == '_'
-                or ($c >= 'a' and $c <='z')
-                or ($c >= 'A' and $c <= 'Z')
-                or ($c >= chr(0x80) and $c <= chr(0xff));
+    return $c == '_'
+        or ($c >= 'a' and $c <='z')
+        or ($c >= 'A' and $c <= 'Z')
+        or ($c >= '0' and $c <= '9')
+        or ($c >= chr(0x80) and $c <= chr(0xff));
 }
