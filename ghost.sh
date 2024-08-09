4d99941459d5f4b8b5c7f5d182c91d0e0fdc5835
liu shiwei <liushiwei@gmail.com>
2024-08-09 15:23:39 +0800

    ghost.sh 修复 /xxx/sys/*

--- ghost.sh
+++ ghost.sh
@@ -1,19 +1,45 @@
 #/bin/bash
-
-dst_file="`uname -m`_`date +%Y%m%d`.tar.gz"
-echo make home/$dst_file
-echo proc/* >/tmp/exclude.list
-echo mnt/* >>/tmp/exclude.list
-echo tmp/* >>/tmp/exclude.list
-echo dev/* >>/tmp/exclude.list
-echo lost+found/* >>/tmp/exclude.list
-echo sys/* >>/tmp/exclude.list
-echo media/* >>/tmp/exclude.list
-echo run/* >>/tmp/exclude.list
-echo home/$dst_file >>/tmp/exclude.list
+aptitude clean
+ver=`cat /etc/debian_version|tr -d "\r\n "`
+ver=${ver%/*}
+echo $ver
+find /  | grep -v \
+ -e "^/proc/" \
+ -e "^/1/" \
+ -e "^/2/" \
+ -e "^/3/" \
+ -e "^/4/" \
+ -e "^/mnt/" \
+ -e "^/tmp/" \
+ -e "^/dev/" \
+ -e "^lost+found/" \
+ -e "^/sys/" \
+ -e "^/media/" \
+ -e "^/run/" \
+ -e "^/root/" \
+ -e "^/home/mips" \
+ -e "^/home/loongson/" \
+ -e "^/home1/" \
+ -e "^/var/tmp/" \
+ -e "^/var/lib/mysql/" \
+ -e "^/var/log/mysql/" \
+ -e "^/var/log/apache2/" \
+ -e "^/var/log/journal/" \
+  > /tmp/all.list
+find /root/.ssh >> /tmp/all.list
 cd /
-if [ "`which pv`" ] ; then
-  tar cp --exclude-from=/tmp/exclude.list * |pv|gzip >/home/$dst_file
+mach=$( uname -m )
+which zstd
+if [ $? == 0 ] ;then
+	gzip=zstd
+        dst_file="${mach}_debian_${ver}_`date +%Y%m%d`.tar.zst"
 else
- tar cpv --exclude-from=/tmp/exclude.list * |gzip >/home/$dst_file
+	gzip=gzip
+        dst_file="${mach}_debian_${ver}_`date +%Y%m%d`.tar.gz"
+fi
+echo make home/$dst_file
+if [ "`which pv`" ] ; then
+  tar c --no-recursion -T /tmp/all.list |pv|$gzip >/home/$dst_file
+else
+ tar cv --no-recursion -T /tmp/all.list |$gzip >/home/$dst_file
 fi
